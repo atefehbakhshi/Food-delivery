ff2a23cadd2961dd5503b6bb97c05185fa3e6c85
Atefeh <at.bakhshi1373@gmail.com>
2023-01-20 11:20:55 +0330

    feat:sticky navigation

--- js/script.js
+++ js/script.js
@@ -38,3 +38,26 @@ allLinksEl.forEach((link) => {
     }
   });
 });
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
